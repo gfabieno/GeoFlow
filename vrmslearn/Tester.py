Apply scaling fix to 1D results plots too

vrmslearn/Tester.py
@@ -219,12 +219,12 @@ class Tester(object):
         label, pred = self.case.label.postprocess(labeld, predd)
 
         for ii, labelname in enumerate(labelnames):
+            if labelname == "ref":
+                vmin, vmax = 0, 1
+            else:
+                vmin = self.case.model.vp_min
+                vmax = self.case.model.vp_max
             if image:
-                if labelname == "ref":
-                    vmin, vmax = 0, 1
-                else:
-                    vmin = self.case.model.vp_min
-                    vmax = self.case.model.vp_max
                 im1 = axs[0, 1 + ii].imshow(pred[labelname],
                                             vmin=vmin,
                                             vmax=vmax,
@@ -245,7 +245,7 @@ class Tester(object):
                 im1, = axs[0, 1 + ii].plot(label[labelname][:, 0][:len(y)], y)
                 im2, = axs[0, 1 + ii].plot(pred[labelname][:, 0][:len(y)], y)
                 axs[0, 1 + ii].set_ylim(np.min(y), np.max(y))
-                axs[0, 1 + ii].set_xlim(-0.1, 1.1)
+                axs[0, 1 + ii].set_xlim(-vmin, vmax)
                 axs[0, 1 + ii].invert_yaxis()
                 axs[0, 1 + ii].set_title(labelname)
                 ims.append(im1)