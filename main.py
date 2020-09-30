8bc1a4338fc16d3afd5ac4d8f0ab10eeaa2ed6af
CloudyOverhead <jerome.simon@hotmail.com>
2020-09-30 14:24:05 -0400

    Move main clause to function

--- main.py
+++ main.py
@@ -10,6 +10,79 @@ from vrmslearn.Tester import Tester
 from vrmslearn.Sequence import Sequence
 
 
+def main(args):
+    logdir = args.logdir
+    batch_size = args.batchsize
+
+    # Define the parameters.
+    case = eval(args.case)()
+
+    # Generate the dataset.
+    if args.training in [0, 2]:
+        case.generate_dataset(ngpu=args.ngpu)
+
+    if args.plot:
+        case.animated_dataset()
+
+    loss_scales = {'ref': args.loss_ref,
+                   'vrms': args.loss_vrms,
+                   'vint': args.loss_vint,
+                   'vdepth': args.loss_vdepth}
+
+    sizes = case.get_dimensions()
+    nn = RCNN2D(input_size=sizes[0],
+                depth_size=sizes[-1][0],
+                batch_size=batch_size,
+                alpha=0.1,
+                beta=0.1,
+                use_peepholes=args.use_peepholes,
+                out_names=loss_scales.keys())
+
+    # Train the model.
+    if args.training in [1, 2]:
+        sequence = Sequence(is_training=True,
+                            case=case,
+                            batch_size=batch_size,
+                            input_size=sizes[0],
+                            depth_size=sizes[-1][0],
+                            out_names=loss_scales.keys())
+        trainer = Trainer(nn=nn,
+                          sequence=sequence,
+                          checkpoint_dir=logdir,
+                          learning_rate=args.lr,
+                          beta_1=args.beta_1,
+                          beta_2=args.beta_2,
+                          epsilon=args.eps,
+                          loss_scales=loss_scales,
+                          use_weights=not args.no_weights)
+        restore_from = tf.train.latest_checkpoint(logdir)
+        trainer.train_model(epochs=args.epochs,
+                            steps_per_epoch=args.steps,
+                            restore_from=restore_from)
+
+    # Test model.
+    if args.training == 3:
+        sequence = Sequence(is_training=False,
+                            case=case,
+                            batch_size=batch_size,
+                            input_size=sizes[0],
+                            depth_size=sizes[-1][0],
+                            out_names=loss_scales.keys())
+        tester = Tester(nn=nn, sequence=sequence, case=case)
+        savepath = os.path.join(case.datatest, "pred")
+        if not os.path.isdir(savepath):
+            os.mkdir(savepath)
+        restore_from = tf.train.latest_checkpoint(logdir)
+        tester.test_dataset(savepath=savepath,
+                            restore_from=restore_from)
+
+        if args.plot:
+            tester.animated_predictions(labelnames=['ref', 'vrms',
+                                                    'vint', 'vdepth'],
+                                        savepath=savepath,
+                                        image="2D" in args.case)
+
+
 if __name__ == "__main__":
 
     # Initialize argument parser
@@ -100,74 +173,4 @@ if __name__ == "__main__":
 
     # Parse the input for training parameters.
     args, unparsed = parser.parse_known_args()
-
-    logdir = args.logdir
-    batch_size = args.batchsize
-
-    # Define the parameters.
-    case = eval(args.case)()
-
-    # Generate the dataset.
-    if args.training in [0, 2]:
-        case.generate_dataset(ngpu=args.ngpu)
-
-    if args.plot:
-        case.animated_dataset()
-
-    loss_scales = {'ref': args.loss_ref,
-                   'vrms': args.loss_vrms,
-                   'vint': args.loss_vint,
-                   'vdepth': args.loss_vdepth}
-
-    sizes = case.get_dimensions()
-    nn = RCNN2D(input_size=sizes[0],
-                depth_size=sizes[-1][0],
-                batch_size=batch_size,
-                alpha=0.1,
-                beta=0.1,
-                use_peepholes=args.use_peepholes,
-                out_names=loss_scales.keys())
-
-    # Train the model.
-    if args.training in [1, 2]:
-        sequence = Sequence(is_training=True,
-                            case=case,
-                            batch_size=batch_size,
-                            input_size=sizes[0],
-                            depth_size=sizes[-1][0],
-                            out_names=loss_scales.keys())
-        trainer = Trainer(nn=nn,
-                          sequence=sequence,
-                          checkpoint_dir=args.logdir,
-                          learning_rate=args.lr,
-                          beta_1=args.beta_1,
-                          beta_2=args.beta_2,
-                          epsilon=args.eps,
-                          loss_scales=loss_scales,
-                          use_weights=not args.no_weights)
-        restore_from = tf.train.latest_checkpoint(args.logdir)
-        trainer.train_model(epochs=args.epochs,
-                            steps_per_epoch=args.steps,
-                            restore_from=restore_from)
-
-    # Test model.
-    if args.training == 3:
-        sequence = Sequence(is_training=False,
-                            case=case,
-                            batch_size=batch_size,
-                            input_size=sizes[0],
-                            depth_size=sizes[-1][0],
-                            out_names=loss_scales.keys())
-        tester = Tester(nn=nn, sequence=sequence, case=case)
-        savepath = os.path.join(case.datatest, "pred")
-        if not os.path.isdir(savepath):
-            os.mkdir(savepath)
-        restore_from = tf.train.latest_checkpoint(args.logdir)
-        tester.test_dataset(savepath=savepath,
-                            restore_from=restore_from)
-
-        if args.plot:
-            tester.animated_predictions(labelnames=['ref', 'vrms',
-                                                    'vint', 'vdepth'],
-                                        savepath=savepath,
-                                        image="2D" in args.case)
+    main(args)
