534878735e8f2cffaccd4327fc133febeb882b3f
CloudyOverhead <jerome.simon@hotmail.com>
2020-10-23 10:11:36 -0400

    Drop useless trainings

--- commands/launch.py
+++ commands/launch.py
@@ -8,6 +8,8 @@ from argparse import Namespace
 from itertools import product
 from importlib import import_module
 
+import numpy as np
+
 from archive import ArchiveRepository
 from vrmslearn.RCNN2D import Hyperparameters
 from Cases_define import *
@@ -82,80 +84,65 @@ def generate_variations(base_params, **variations):
     return hyperparams
 
 
-hyperparams = generate_variations(Hyperparameters(),
-                                  freeze_to=[None, (None, "ref", "vrms")],
-                                  encoder_kernels=[[[15, 1, 1],
-                                                    [1, 9, 1],
-                                                    [15, 1, 1],
-                                                    [1, 9, 1]],
-                                                   [[15, 1, 3],
-                                                    [1, 9, 3],
-                                                    [15, 1, 3],
-                                                    [1, 9, 3]]],
-                                  encoder_dilations=[[[1, 1, 1],
-                                                      [1, 1, 1],
-                                                      [1, 1, 1],
-                                                      [1, 1, 1]],
-                                                     [[1, 1, 2],
-                                                      [1, 1, 2],
-                                                      [1, 1, 2],
-                                                      [1, 1, 2]]],
-                                  rcnn_kernel=[[15, 3, 1], [15, 3, 3]],
-                                  rcnn_dilation=[[1, 1, 1], [1, 1, 2]],
-                                  decode_kernel=[[1, 1], [1, 5]])
-checkpoint_1d = "logs/optimize-2D-kernels/a775455/2/model/0140.ckpt"
-checkpoint_1d = realpath(checkpoint_1d)
-optimize(params=hyperparams,
-         case=Case2Dtest_complexity(),
-         epochs=(100, 100, 50),
-         steps=20,
-         lr=[.0002, .00008],
-         beta_1=.9,
-         beta_2=.98,
-         eps=1e-5,
-         batchsize=4,
-         loss_ref=(.5, .0, .0),
-         loss_vrms=(.5, .7, .0),
-         loss_vint=(.0, .3, 1.),
-         loss_vdepth=(.0, .0, .0),
-         nmodel=1,
-         ngpu=2,
-         noise=0,
-         plot=0,
-         no_weights=False,
-         restore_from=[None, (checkpoint_1d, None, None)])
+def drop_useless(hyperparams):
+    to_keep = np.ones(len(hyperparams), dtype=bool)
+    for i, p in enumerate(hyperparams):
+        is_dilation_useful = True
+        for key, value in p.__dict__.items():
+            if value is None or "dilation" not in key:
+                continue
+            value = np.array(value)
+            if (value != 1).any():
+                attrib = key.split("_")[0]
+                s_or_not = "s" if key[-1] == "s" else ""
+                attrib = attrib + "_kernel" + s_or_not
+                idx_dilation = np.nonzero(value != 1)
+                idx_dilation = idx_dilation
+                other_value = getattr(p, attrib)
+                other_value = np.array(other_value)
+                if not (other_value[idx_dilation] != 1).all():
+                    is_dilation_useful = False
+        to_keep[i] = is_dilation_useful
+    hyperparams = np.array(hyperparams, dtype=object)
+    hyperparams = hyperparams[to_keep]
+    return list(hyperparams)
+
 
 hyperparams = Hyperparameters()
+hyperparams.freeze_to = (None, "ref", "vrms")
+all_hparams = [hyperparams]
+hyperparams = Hyperparameters()
+hyperparams.freeze_to = (None, "ref", "vrms")
+hyperparams.encoder_kernels = [[15, 1, 3],
+                               [1, 9, 3],
+                               [15, 1, 3],
+                               [1, 9, 3]]
+all_hparams.extend(generate_variations(hyperparams,
+                                       encoder_dilations=[[[1, 1, 1],
+                                                           [1, 1, 1],
+                                                           [1, 1, 1],
+                                                           [1, 1, 1]],
+                                                          [[1, 1, 2],
+                                                           [1, 1, 2],
+                                                           [1, 1, 2],
+                                                           [1, 1, 2]]],
+                                       rcnn_kernel=[[15, 3, 1], [15, 3, 3]],
+                                       rcnn_dilation=[[1, 1, 1], [1, 1, 2]]))
+
 hyperparams.use_cnn = True
-hyperparams.cnn_kernel = [1, 3]
+hyperparams.cnn_kernel = [1, 5]
 hyperparams.cnn_filters = 32
 hyperparams.cnn_dilation = [1, 2]
-hyperparams = generate_variations(hyperparams,
-                                  freeze_to=[None, (None, "ref", "vrms")],
-                                  encoder_kernels=[[[15, 1, 1],
-                                                    [1, 9, 1],
-                                                    [15, 1, 1],
-                                                    [1, 9, 1]],
-                                                   [[15, 1, 3],
-                                                    [1, 9, 3],
-                                                    [15, 1, 3],
-                                                    [1, 9, 3]]],
-                                  encoder_dilations=[[[1, 1, 1],
-                                                      [1, 1, 1],
-                                                      [1, 1, 1],
-                                                      [1, 1, 1]],
-                                                     [[1, 1, 2],
-                                                      [1, 1, 2],
-                                                      [1, 1, 2],
-                                                      [1, 1, 2]]],
-                                  rcnn_kernel=[[15, 3, 1], [15, 3, 3]],
-                                  rcnn_dilation=[[1, 1, 1], [1, 1, 2]],
-                                  decode_kernel=[[1, 1], [1, 5]])
-optimize(params=hyperparams,
+all_hparams.append(deepcopy(hyperparams))
+all_hparams = drop_useless(all_hparams)
+
+checkpoint_1d = "logs/optimize-2D-kernels/a775455/2/model/0140.ckpt"
+checkpoint_1d = realpath(checkpoint_1d)
+optimize(params=all_hparams,
          case=Case2Dtest_complexity(),
-         epochs=(100, 100, 50),
+         epochs=(80, 80, 50),
          steps=20,
-         lr=[.0002, .00008],
+         lr=.0002,
          beta_1=.9,
          beta_2=.98,
          eps=1e-5,
@@ -169,4 +156,4 @@ optimize(params=hyperparams,
          noise=0,
          plot=0,
          no_weights=False,
-         restore_from=[None, (checkpoint_1d, None, None)])
+         restore_from=(checkpoint_1d, None, None))
