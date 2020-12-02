605d72cd2b8c32e46118f817e5c8126da3760f48
CloudyOverhead <jerome.simon@ete.inrs.ca>
2020-12-02 18:52:54 -0500

    Account for batches in losses

--- GeoFlow/Losses.py
+++ GeoFlow/Losses.py
@@ -11,7 +11,7 @@ def ref_loss():
     Get the loss function for the reflection prediction.
     """
     def loss(label, output):
-        label, weight = label[0], label[1]
+        label, weight = label[:, 0], label[:, 1]
         # Logistic regression of zero offset time of reflections.
         weight = tf.expand_dims(weight, -1)
         output = output * weight
@@ -20,7 +20,7 @@ def ref_loss():
 
         loss = tf.nn.softmax_cross_entropy_with_logits(logits=output,
                                                        labels=label)
-        loss = tf.reduce_mean(loss)
+        loss = tf.reduce_mean(loss, axis=[])
         return loss
 
     return loss
@@ -40,31 +40,37 @@ def v_compound_loss(alpha=0.2, beta=0.1):
     fact1 = 1 - alpha - beta
 
     def loss(label, output):
-        label, weight = label[0], label[1]
+        label, weight = label[:, 0], label[:, 1]
         losses = []
 
         # Calculate mean squared error
         if fact1 > 0:
-            num = tf.reduce_sum(weight * (label-output)**2)
-            den = tf.reduce_sum(weight * label**2)
-            losses.append(fact1 * num / den)
+            num = tf.reduce_sum(weight * (label-output)**2, axis=[1, 2])
+            den = tf.reduce_sum(weight * label**2, axis=[1, 2])
+            loss = tf.reduce_sum(num / den)
+            losses.append(fact1 * loss)
 
         #  Calculate mean squared error of the z derivative
         if alpha > 0:
             dlabel = label[:, 1:, :] - label[:, :-1, :]
             dout = output[:, 1:, :] - output[:, :-1, :]
-            num = tf.reduce_sum(weight[:, :-1, :] * (dlabel-dout)**2)
-            den = tf.reduce_sum(weight[:, :-1, :] * dlabel**2) + 1E-6
-            losses.append(alpha * num / den)
+            num = tf.reduce_sum(weight[:, :-1, :] * (dlabel-dout)**2,
+                                axis=[1, 2])
+            den = tf.reduce_sum(weight[:, :-1, :] * dlabel**2,
+                                axis=[1, 2]) + 1E-6
+            loss = tf.reduce_sum(num / den)
+            losses.append(alpha * loss)
 
         # Minimize gradient (blocky inversion)
         if beta > 0:
-            num = (
-                tf.norm(output[:, 1:, :] - output[:, :-1, :], ord=1)
-                + tf.norm(output[:, :, 1:] - output[:, :, :-1], ord=1)
-            )
-            den = tf.norm(output, ord=1) / 0.02
-            losses.append(beta * num / den)
+            num = tf.norm(output[:, 1:, :] - output[:, :-1, :], ord=1,
+                          axis=[1, 2])
+            if output.get_shape()[-1] != 1:
+                num += tf.norm(output[:, :, 1:] - output[:, :, :-1], ord=1,
+                               axis=[1, 2])
+            den = tf.norm(output, ord=1, axis=[1, 2]) / 0.02
+            loss = tf.reduce_sum(num / den)
+            losses.append(beta * loss)
 
         return tf.reduce_sum(losses)
 
