556551fb434600807019eea11791b7a5165e24a4
CloudyOverhead <jerome.simon@ete.inrs.ca>
2020-12-07 05:20:12 -0500

    Prototype using `ray[tune]`

--- AutomatedTraining/AutomatedTraining.py
+++ AutomatedTraining/AutomatedTraining.py
@@ -15,20 +15,26 @@ ongoing training. `optimize` automatically fetches the archived main script.
 
 import sys
 from copy import deepcopy
-from argparse import Namespace
-from itertools import product
+from argparse import Namespace, ArgumentParser
 from importlib import import_module
 from typing import Callable
 
-import numpy as np
+from ray import tune
 
 from Archive import ArchiveRepository
-from GeoFlow.RCNN2D import Hyperparameters, RCNN2D
+from GeoFlow import RCNN2D
+from GeoFlow.GeoDataset import GeoDataset
 
 
-def chain(main: Callable, **args):
+def chain(main: Callable,
+          architecture: RCNN2D.RCNN2D,
+          params: Namespace,
+          dataset: GeoDataset,
+          logdir: str = "./logs",
+          ngpu: int = 1,
+          **config):
     """
-    Call `main` a succession of times as implied by `args`.
+    Call `main` a succession of times as implied by `config`.
 
     :param main: A callable that oversees training and testing (i.e.
                  `..main.main`)
@@ -39,7 +45,6 @@ def chain(main: Callable, **args):
     Sample usage:
         from main import main
         params = Hyperparameters()
-        params.epochs = (100, 100, 50),
         params.loss_scales = ({'ref': .5, 'vrms': .5,
                                'vint': .0, 'vdepth': .0},
                               {'ref': .0, 'vrms': .7,
@@ -53,144 +58,63 @@ def chain(main: Callable, **args):
               architecture=RCNN2D,
               ngpu=2)
     Output:
-        A 3-step training with different quantities of epochs and losses.
+        A 3-step training with different losses.
     """
-    if "training" in args.keys():
-        raise ValueError("Using `chain` implies training.")
-    hyperparams = args["params"]
-    if isinstance(hyperparams.freeze_to, tuple):
-        args["params"] = generate_variations(hyperparams,
-                                             freeze_to=hyperparams.freeze_to)
-        args["params"] = tuple(args["params"])
-    parameters = {key: value
-                  for key, value in args.items()
-                  if isinstance(value, tuple)}
-    if parameters:
-        qty_segments = max([len(sequence) for sequence in parameters.values()])
+    params = deepcopy(params)
+    for param_name, param_value in config:
+        setattr(params, param_name, param_value)
+
+    to_chain = {}
+    for param_name, param_value in params.__dict__.items():
+        if isinstance(param_value, tuple):
+            to_chain[param_name] = param_value
+
+    if to_chain:
+        qties_segments = [len(param) for param in params.values()]
+        qty_segments = qties_segments[0]
+        assert all([qty == qty_segments for qty in qties_segments]), (
+            "A hyperparameter sequence has a different length."
+        )
     else:
         qty_segments = 1
-    constants = {key: [value]*qty_segments
-                 for key, value in args.items()
-                 if not isinstance(value, tuple)}
-    parameters.update(constants)
-    keys = parameters.keys()
-    values = parameters.values()
-    try:
-        for current_values in zip(*values):
-            current_parameters = {key: value for key, value
-                                  in zip(keys, current_values)}
-            args = Namespace(training=1, **current_parameters)
-            main(args)
-    except Exception as exception:
-        print("Could not do or finish training:")
-        print(exception)
+
+    for segment in range(qty_segments):
+        current_params = deepcopy(params)
+        for param_name, param_value in to_chain:
+            current_params[param_name] = param_value[segment]
+        main(current_params)
 
 
-def optimize(**args):
+def optimize(architecture: RCNN2D.RCNN2D,
+             params: Namespace,
+             dataset: GeoDataset,
+             logdir: str = "./logs",
+             ngpu: int = 1,
+             **config):
     """
-    Call `chain` for all combinations of `args`.
+    Call `chain` for all combinations of `chain`.
 
-    :param args: Key-value pairs of argument names and values. Values
-                 that are lists will be iterated upon.
+    :param args: Key-value pairs of argument names and values that will be
+                 iterated upon.
 
     Sample usage:
-        optimize(params=Hyperparameters(),
+        optimize(architecture=RCNN2D.RCNN2D,
+                 params=Hyperparameters(),
                  dataset=Dataset1Dsmall(),
                  lr=[.0008, .0002])
     Output:
         Two calls to `chain` with different learning rates.
     """
-    if "logdir" in args.keys():
-        raise ValueError("`optimize` manages checkpoint directories by "
-                         "itself.")
-    if "training" in args.keys():
-        raise ValueError("Using `optimize` implies training.")
-    parameters = {key: value
-                  for key, value in args.items()
-                  if isinstance(value, list)}
-    constants = {key: [value]
-                 for key, value in args.items()
-                 if not isinstance(value, list)}
-    parameters.update(constants)
-    keys = parameters.keys()
-    values = parameters.values()
-    for current_values in product(*values):
-        current_parameters = {key: value for key, value
-                              in zip(keys, current_values)}
-        with ArchiveRepository() as archive:
-            archive.write(str(current_parameters))
-            main = import_module("main").main
-            chain(main, logdir=archive.model, **current_parameters)
-            del sys.modules["main"]
-            del main
-
-
-def generate_variations(base_params: Hyperparameters, **variations):
-    """
-    Generate variations of an `Hyperparameters` object.
-
-    :param base_params: A base `Hyperparameters` object.
-    :param variations: Values with which to overwrite the attributes of
-                       `base_params` with. The keys are attribute names and the
-                       values will be iterated upon using a cartesian product.
-
-    Sample usage:
-        hyperparams = Hyperparameters()
-        generate_variations(hyperparams, rcnn_kernel=[[15, 3, 1],
-                                                      [15, 3, 3]])
-    Output:
-        A list of two `Hyperparameters` objects with different `rcnn_kernel`
-        attributes.
-    """
-    hyperparams = []
-    keys = variations.keys()
-    values = variations.values()
-    for current_values in product(*values):
-        current_hyperparams = deepcopy(base_params)
-        for key, value in zip(keys, current_values):
-            setattr(current_hyperparams, key, value)
-        hyperparams.append(current_hyperparams)
-    return hyperparams
-
-
-def drop_useless(hyperparams: Hyperparameters):
-    """
-    Drop useless hyperparameters combinations.
-
-    Useless hyperparameters combinations have diltations in dimensions of
-    length 1.
-
-    :param hyperparams: A list of `Hyperparameters` objects.
-
-    Sample usage:
-        hyperparams = Hyperparameters()
-        hyperparams.rcnn_kernel = [15, 3, 1]
-        hyperparams.rcnn_dilation = [1, 1, 2]
-        drop_useless([hyperparams])
-    Output:
-        An empty list.
-    """
-    to_keep = np.ones(len(hyperparams), dtype=bool)
-    for i, p in enumerate(hyperparams):
-        is_dilation_useful = True
-        for key, value in p.__dict__.items():
-            if value is None or "dilation" not in key:
-                continue
-            value = np.array(value)
-            if (value != 1).any():
-                attrib = key.split("_")[0]
-                s_or_not = "s" if key[-1] == "s" else ""
-                attrib = attrib + "_kernel" + s_or_not
-                idx_dilation = np.nonzero(value != 1)
-                idx_dilation = idx_dilation
-                other_value = getattr(p, attrib)
-                other_value = np.array(other_value)
-                if not (other_value[idx_dilation] != 1).all():
-                    is_dilation_useful = False
-        to_keep[i] = is_dilation_useful
-    hyperparams = np.array(hyperparams, dtype=object)
-    hyperparams = hyperparams[to_keep]
-    return list(hyperparams)
+    with ArchiveRepository() as archive:
+        main = import_module("main").main
+        logdir = archive.model
+        tune.run(lambda config: chain(main, architecture, params, dataset,
+                                      logdir, ngpu, **config),
+                 num_samples=1,
+                 resources_per_trial={"gpu": ngpu},
+                 config=config)
+        del sys.modules["main"]
+        del main
 
 
 if __name__ == "__main__":
