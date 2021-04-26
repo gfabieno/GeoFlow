a7d8f9502b0b097b3c31e6cede334233e72b2ccc
CloudyOverhead <jerome.simon@geolearn.ca>
2021-04-26 10:00:57 -0400

    Import all submodules in public API

--- GeoFlow/__init__.py
+++ GeoFlow/__init__.py
@@ -1,3 +1,11 @@
+import glob
+from os.path import dirname, basename, isfile, join
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [basename(f)[:-3] for f in modules
+           if isfile(f) and not f.endswith('__init__.py')]
+
+from . import *
 from .GeoDataset import GeoDataset
 from .EarthModel import EarthModel
 from .GraphIO import *
