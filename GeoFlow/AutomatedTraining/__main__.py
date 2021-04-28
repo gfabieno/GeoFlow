5aef37f37d2cd2e5d9d8e4921456ba0a1ef7f72e
CloudyOverhead <jerome.simon@geolearn.ca>
2021-04-28 09:45:05 -0400

    Use training hyperparameters in `AutomatedTraining`

--- GeoFlow/AutomatedTraining/__main__.py
+++ GeoFlow/AutomatedTraining/__main__.py
@@ -9,7 +9,7 @@ args, config = parser.parse_known_args()
 config = {name[2:]: eval(value)
           for name, value in zip(config[::2], config[1::2])}
 args.nn = getattr(DefinedNN, args.nn)
-args.params = getattr(DefinedNN, args.params)(is_training=args.train)
+args.params = getattr(DefinedNN, args.params)(is_training=True)
 args.dataset = getattr(DefinedDataset, args.dataset)(args.noise)
 
 if args.debug:
